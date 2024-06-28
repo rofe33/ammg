2640a3cd7ff9d105c0a46d222f9ef1b2285ecd85
rofe33 <r3veal@protonmail.ch>
2024-06-28 21:58:31 +0300

    Version bump to v0.2.1

--- src/ammg/ammg.py
+++ src/ammg/ammg.py
@@ -49,7 +49,7 @@ def parse_arguments():
         '-v',
         '--version',
         action='version',
-        version='%(prog)s 0.2.0',
+        version='%(prog)s 0.2.1',
     )
 
     subparsers = parser.add_subparsers(
